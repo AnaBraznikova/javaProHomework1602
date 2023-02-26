4b8e3903afd3479ff0c35e08e6b01d4fce0300b1
AnaBraznikova <116310193+AnaBraznikova@users.noreply.github.com>
2023-02-26 20:04:07 +0100

    Update Apple.java

--- homework1602/Apple.java
+++ homework1602/Apple.java
@@ -22,7 +22,6 @@ public class Apple extends Phone {
     @Override
     public void receiveCall() {
         //super.receiveCall();
-        String number = null;
         System.out.println("Phone " + name + " receive call from " + "+" + getRandomNumber());
     }
 
